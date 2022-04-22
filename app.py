4674fa91673d6244f9349566682b38296c8e3bd2
nebil2016 <seidy2004@gmail.com>
2022-04-22 14:35:33 -0400

    uploading challenge codes

--- app.py
+++
@@ -1,9 +0,0 @@
-# To import the Flask dependency, add the following to your code
-from flask import Flask
-# Add the following to your code to create a new Flask instance called app
-app = Flask(__name__)
-# our Flask app has been created—let's create our first route!
-@app.route('/')
-def hello_world():
-    return 'Hello world'
-    
